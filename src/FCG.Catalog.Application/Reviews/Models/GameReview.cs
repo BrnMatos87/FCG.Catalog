b7ec48888fad8c62480439f98e9ede1f387de109
Bruno Matos <brn_matos@hotmail.com>
2026-09-14 23:38:21 -0300

    Revisão e ajustes finais.

--- src/FCG.Catalog.Application/Reviews/Models/GameReview.cs
+++ src/FCG.Catalog.Application/Reviews/Models/GameReview.cs
@@ -1,11 +1,16 @@
-﻿namespace FCG.Catalog.Application.Reviews.Models;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FCG.Catalog.Application.Reviews.Models;
 
 public class GameReview
 {
     public string Id { get; set; } = string.Empty;
 
+    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid GameId { get; set; }
 
+    [BsonGuidRepresentation(GuidRepresentation.Standard)]
     public Guid UserId { get; set; }
 
     public int Rating { get; set; }
